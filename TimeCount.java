895ff8f215aa2c1569bcb5aaba03ed5139774f72
JanaZhak <67675843+JanaZhak@users.noreply.github.com>
2020-08-05 11:56:31 +0300

    Мой черновой вариант
    Вариант на проверку лежит пока в ветке master

--- TimeCount.java
+++ TimeCount.java
@@ -1,5 +1,8 @@
-public class TimeCount {
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
+public class TimeCount {
+    // private int timeStart;
+    //  private int timeFinish;
     private int hour;
     private int min;
     private int sec;
@@ -7,70 +10,72 @@ public class TimeCount {
     public TimeCount() {
     }
 
-    public TimeCount(int sec, int min, int hour) {   
-        if (sec < 0 || min < 0 || hour < 0) {
-            System.out.println("Please, enter correct time");
-        } else {
-            this.sec = sec;
-            this.min = min;
-            this.hour = hour;
-           
-        }
+    public TimeCount(int sec, int min, int hour) {
+        this.sec = sec;
+        this.min = min;
+        this.hour = hour;
     }
 
-    public TimeCount(int seconds) { //конструктор, получающий  часы, минуты и секунды по отдельности
-        if (seconds < 0) {
-            System.out.println("Please, enter correct time");
-        } else
-            hour = seconds / 3600;
-        min = (seconds - hour * 3600) / 60;
-        sec = seconds - hour * 3600 - min * 60;
-    }
-
-    // public TimeCount(int hour, int min, int sec) {  // конструктор, получающий общее кол-во секунд
-    // sec =  (hour * 60 + min) * 60 + sec;
-    // }
-
-
-    void printTimeCountInfo() {  //метод для вывода данных
-
-        System.out.println(sec + " second(s) " + min + " minute(s) " + hour + " hour(s)");
+    void printTimeCountInfo() {
+        System.out.println(sec + " seconds " + min + " minutes " + hour + " hour");
     }
 
 
-    int totalSec() {   // метод для получения полного количества секунд в объекте
-
+    int totalSec() {
         return (hour * 60 + min) * 60 + sec;
     }
 
-//   public boolean equal(finish, TimeCount start) { //  ????? метод сравнения двух объектов ????
-//                if (start.totalSec() == finish.totalSec()) {
-//            return true;}
-//                else{
-//                    return false;}
-
-
-    public static void main(String[] args) {
-        TimeCount start = new TimeCount(25, 27, 1);
-        TimeCount finish = new TimeCount(8906);
-        start.printTimeCountInfo();
-        finish.printTimeCountInfo();
-        int seconds = finish.totalSec() - start.totalSec();
-        System.out.println(start.equals(finish));
-        if (seconds < 0) {
+    public void equal() {
+        if (totalSec() < 0) {
             System.out.println("Please, enter correct time");
         } else {
-            System.out.print("Промежуток времени составляет " + seconds + " секунд," + " что составляет " + "");
+            System.out.println(totalSec());
         }
+    }
 
 
-        TimeCount Seconds = new TimeCount(seconds);
-        Seconds.printTimeCountInfo();
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int total) {
+        this.hour = total/60/60;
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+    public void setMin(int total) {
+        this.min = (total - sec%60)/60;
+        ;
+    }
+
+    public int getSec(){
+        return sec;
+    }
+
+    public void setSec(int total) {
+        this.sec = total%60;
+        ;
+    }
+
+    public static void main(String[] args) {
+        TimeCount Start = new TimeCount(26, 25, 1);
+        TimeCount Finish = new TimeCount(26, 25, 2);
+        Start.printTimeCountInfo();
+        Finish.printTimeCountInfo();
+        int total = Finish.totalSec() - Start.totalSec();
+
+        System.out.println("Промежуток времени составляет " + total + " секунд," + " что составляет " + "");
+
+        System.out.println(Start.equals(Finish));
+
+
 
 
     }
 }
-
 
 //   Создать класс и объекты описывающие промежуток времени. Сам промежуток
 //    в классе должен задаваться тремя свойствами: секундами, минутами, часами.
