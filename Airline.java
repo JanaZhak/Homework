cb01aa623de1c50d724f90dfcf0beb1b02108c65
JanaZhak <67675843+JanaZhak@users.noreply.github.com>
2020-08-25 21:05:08 +0300

    Add files via upload

---
+++ Airline.java
@@ -0,0 +1,70 @@
+public class Airline {
+    private String destination; //пункт назначения
+    private String flightNumber; //номер рейса
+    private String typeOfAircraft; //тип самолета
+    private String departure; // время вылета
+    private String weekday; //день недели
+
+///конструктор
+    public Airline(String destination, String flightNumber, String typeOfAircraft, String departure, String weekday) {
+        this.destination = destination;
+        this.flightNumber = flightNumber;
+        this.typeOfAircraft = typeOfAircraft;
+        this.departure = departure;
+        this.weekday = weekday;
+
+    }
+
+    //getters
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getTypeOfAircraft() {
+        return typeOfAircraft;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public String getWeekday() { return weekday;}
+//setters
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public void setTypeOfAircraft(String typeOfAircraft) {
+        this.typeOfAircraft = typeOfAircraft;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public void setWeekday(String weekday) {
+        this.weekday = weekday;
+    }
+    //methods
+    public String toString()
+    { return "Flight Information\nNumber Of Flight: " + setFlightNumber() +"\nDestination: " + getDeparture();
+}
+
+
+
+
+//1)Создать класс Airline: Пункт назначения, Номер рейса, Тип самолета, Время вылета, Дни
+//недели.
+//Создать массив объектов. Вывести:
+//a) список рейсов для заданного пункта назначения;
+//b) список рейсов для заданного дня недели;
+//c) список рейсов для заданного дня недели, время вылета для которых
+//больше заданного
