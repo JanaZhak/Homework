c3d4e3c1dcbe4f7b3e6e5ef1f0abfde483155529
JanaZhak <67675843+JanaZhak@users.noreply.github.com>
2020-08-26 21:10:59 +0300

    Add files via upload

---
+++ Schedule.java
@@ -0,0 +1,19 @@
+package AirlineTask;
+public class Schedule {
+
+
+
+
+    public void main(String[] args) {
+        Airline[0] = new Airline("Roma", "001", "Boing", "10.00 PM", "Sunday");
+        Airline[1] = new Airline("Paris", "002", "Boing", "11.00 PM", "Monday");
+        Airline[2] = new Airline("Minsk", "003", "TU", "12.00 PM", "Tuesday");
+        Airline[3] = new Airline("Moscow", "004", "Boing", "13.00 PM", "Wednesday");
+        Airline[4] = new Airline("Warsaw", "005", "TU", "14.00 PM", "Saturday");
+
+        if (this.destination == Roma){
+            System.out.println();
+        }
+                {
+        }
+    }
